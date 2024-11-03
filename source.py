01df2166c70d1e4a7187344116e6126ef2958cd9
CodebyKumar <153998849+CodebyKumar@users.noreply.github.com>
2024-11-03 17:08:41 +0530

    Added view and delete option

--- source.py
+++ source.py
@@ -2,6 +2,7 @@
 import csv
 import os
 from datetime import date
+import pprint
 
 # Functions
 # add expense to the .csv file
@@ -15,7 +16,7 @@ def addexp():
     # exp['Date']= input("Enter the date: ")
     exp['Date']= date.today()
     exp['Category']= input("Enter the category: ")
-    exp['Description']= input("Enter the description(Name,Quantity): ")
+    exp['Description']= input("Enter the description(Name-Quantity): ")
     exp['Amount']= int(input("Enter the amount: "))
 
     exp_list=[exp]
@@ -28,8 +29,9 @@ def addexp():
             writer.writeheader()
         
         writer.writerows(exp_list)
+        print("Expense Added Successfully!")
 
-# sort expense based on filedname
+# sort expense based on fieldname
 def sort(fieldname):
     with open('ExpenseTrack/exp_source.csv','r', newline='') as file:
         reader = csv.DictReader(file)
@@ -40,7 +42,53 @@ def sort(fieldname):
         for row in data:
             print(row)
 
+# view expense based on date
+def viewexp():
+    with open('ExpenseTrack/exp_source.csv','r', newline='') as file:
+        reader = csv.DictReader(file)
+        choice=int(input("1. View all expenses\n2. View expenses based on date\nEnter your choice: "))
+        if choice==1:
+            data=list(reader)
+            pprint.pprint(data)
+        elif choice==2:
+            date=input("Enter the date to view the expense(YYYY-MM-DD): ")
+            count=0
+            for row in reader :
+                if row['Date']==date:
+                    o_data=list(row.values())
+                    print(f'{count+1}. {o_data[1]}\t{o_data[2]}\t{o_data[3]}\n')
+                    count+=1
+            if count==0:
+                print("No Record Found!")
+
+# delete an expense
+# need to add an option to delete expense based on date
+def delexp():
+    with open('ExpenseTrack/exp_source.csv','r', newline='') as file:
+        reader = csv.DictReader(file)
+        choice=int(input("1. Delete all expenses\n2. Delete expense based on row number\nEnter your choice: "))
+        if choice==1:
+            with open('ExpenseTrack/exp_source.csv','w', newline='') as file:
+                writer = csv.DictWriter(file,fieldnames=["Date", "Category", "Description", "Amount"])
+                writer.writeheader()
+            print("All Expenses Deleted Successfully!")
+        elif choice==2:
+            data=list(reader)
+            pprint.pprint(data)
+            print("Enter the row number to delete the expense: ")
+            row=int(input())
+            data.pop(row-1)
+            print(data)
+            with open('ExpenseTrack/exp_source.csv','w', newline='') as file:
+                writer = csv.DictWriter(file,fieldnames=["Date", "Category", "Description", "Amount"])
+                writer.writeheader()
+                writer.writerows(data)
+            print("Expense Deleted Successfully!")
+
+    
+            
 
 # Execution part
-addexp()
-sort('Category')
+# addexp()
+# viewexp()
+# delexp()
